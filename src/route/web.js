735a5d590e974673f8edb0dff9fe2f660e2d087e
Le Kiem <lvkkiem2002@gmail.com>
2023-01-07 23:18:27 +0700

    CRUD

--- src/route/web.js
+++ src/route/web.js
@@ -1,12 +1,16 @@
 import express from "express";
-import homeController from '../controller/homeController'
+import homeController from "../controller/homeController";
 
 let router = express.Router();
 
 const initWebRoute = (app) => {
   router.get("/", homeController.getHomepage);
-  router.get('/detail/user/:id', homeController.getDetail);
-  router.post('/create-new-user', homeController.createNewUser)
+  router.get("/detail/user/:id", homeController.getDetail);
+  router.post("/create-new-user", homeController.createNewUser);
+  router.post("/delete-user", homeController.deleteUser);
+
+  router.get('/edit-user/:id', homeController.editPage)
+  router.post('/update-user', homeController.updateUser)
 
   return app.use("/", router);
 };
